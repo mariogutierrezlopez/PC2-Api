d3958461539146c55d1ad9f4f8af4a38713c5c76
Christian Jonathan Yang Gonzalez <christianj_yang@icloud.com>
2024-05-28 23:43:08 +0200

    terminado la api de gestion de usuario

--- app/Http/Controllers/GestionUsuarioController.php
+++ app/Http/Controllers/GestionUsuarioController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\GestionUsuario;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\GestionUsuarioResource;
 use Illuminate\Support\Facades\Validator;
 
 class GestionUsuarioController extends Controller
-{public function index()
+{
+    public function index()
     {
         return GestionUsuario::all();
     }
@@ -16,7 +18,20 @@ class GestionUsuarioController extends Controller
     public function update(Request $request, $id)
     {
         $usuario = GestionUsuario::findOrFail($id);
+        
+        $validator = Validator::make($request->all(), [
+            'pass' => 'sometimes|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        if ($request->has('pass')) {
+            $request->merge(['pass' => Hash::make($request->pass)]);
+        }
+
         $usuario->update($request->all());
-        return $usuario;
+        return response()->json(new GestionUsuarioResource($usuario), 200);
     }
 }
